b9e573702aca5860b2aae73b4beb4bd7ee8dcd67
200~mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-02-25 13:25:19 +0200

    Implicitly-Type Local Variables [var - dynamic]

--- Session1/Program.cs
+++ Session1/Program.cs
@@ -2,9 +2,48 @@
 {
     internal class Program
     {
+        // can't use var as return type or Parameter
+        //public static var Print(var X)
+        //{
+        //    return X;
+        //} 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Implicitly-Type Local Variables [Var-Dynamic]
+
+            // Var
+            var Data01 = 1;
+
+            // Compiler Can Detect The Datatype of the local variable based on initial value, At Compilation Time
+            // must be initialization
+            // can't initialization the local variable with null
+            // can't change datatype of the local variable after initialization
+
+            // Data01=12;//Invalid
+
+            // dynamic : like var in JS
+            dynamic Data02 = "Ahmed";
+             Data02 = null;
+
+            // CLR Detect the Datatype of the local variable based on last value, At Runtime
+            // Don't Need To Be Initialized
+            // Can initialized the local variable with Null
+            // can change datatype of the local variable after initialization
+            // can use dynamic keyword as return type or Parameter
+            // Be Carefull when use Dynamic
+            // Like var in JS Or Object in C#
+            Console.WriteLine(Data02.GetType().Name);
+            Data02 = 12;
+            Data02 = 1.4;
+            Data02 = 1.5f;
+            Data02 = true;
+
+            //var x=()=> Console.WriteLine("hello World");
+            var x = delegate(){ Console.WriteLine("hello world"); };
+            
+            //dynamic x = delegate(){ Console.WriteLine("hello world"); };
+
+
         }
     }
 }
